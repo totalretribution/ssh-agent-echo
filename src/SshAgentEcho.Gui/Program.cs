0259f24172b68f6fd3581eed2a59f2d2260992e9
totalretribution <totalretribution@users.noreply.github.com>
2026-02-19 22:10:58 +0000

    Installed Velopack

--- src/SshAgentEcho.Gui/Program.cs
+++ src/SshAgentEcho.Gui/Program.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Velopack;
 
 using SshAgentEcho.Core;
 
@@ -15,8 +16,20 @@ public partial class App : Application {
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) {
+        try {
+            // It's important to Run() the VelopackApp as early as possible in app startup.
+            VelopackApp.Build().Run();
+
+            // Now it's time to run Avalonia
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        } catch (Exception ex) {
+            string message = "Unhandled exception: " + ex.ToString();
+            Console.WriteLine(message);
+            throw;
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
